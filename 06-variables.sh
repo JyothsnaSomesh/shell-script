f6a486cca1eb598e3781546eccdb5e144b96cc6b
JyothsnaSomesh <joesomesh@gmail.com>
2023-12-15 09:52:25 -0600

    added new

--- 06-variables.sh
+++ 06-variables.sh
@@ -1,6 +1,11 @@
 #1/bin/bash
 
+echo "please enter your username"
+
+read username
+echo "username is $username"
+
 echo "please enter your password"
 
 read -s password
-echo "password is $password"
+echo " password is $password"
