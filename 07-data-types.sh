8d8816a7e5160e222bd325f3f4c4074584b3063e
JyothsnaSomesh <joesomesh@gmail.com>
2023-12-15 12:52:12 -0600

    added new

--- 07-data-types.sh
+++ 07-data-types.sh
@@ -4,7 +4,8 @@ number1=$1
 number2=$2
 number3=$3
 number4=$4
+number10=$10
 
-sum=$(($number1+$number2+$number3+$number4))
+sum=$(($number1+$number2+$number3+$number4+$number10))
 
 echo "Total::$sum"
