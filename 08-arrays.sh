0da3127c7a449ef87e64b5d9ae076371b19b152a
JyothsnaSomesh <joesomesh@gmail.com>
2023-12-15 19:48:15 -0600

    added new

---
+++ 08-arrays.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+Fruits= "apple" "Banana" "Mango" "Orange" "strawberry"
+
+echo "first value : ${Fruits[0]}"
+
+echo "Second value : ${Fruits[1]}"
+
+echo "Third value : ${Fruits[2]}"
+
+echo "first value : ${Fruits[3]}"
