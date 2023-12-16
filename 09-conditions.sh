76c7397cb20c619ac7bc7e93ae730a2f81230d36
JyothsnaSomesh <joesomesh@gmail.com>
2023-12-16 11:06:52 -0600

    new test

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -4,7 +4,7 @@ Number=$1
 
 if [ $NUMBER -gt 100 ]
 then
-echo "given number  $NUMBER greater than 100"
+echo "given number $NUMBER greater than 100"
 else
 echo "given number $NUMBER less than 100"
 
