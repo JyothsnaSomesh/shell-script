a5b3cc6a3d997ee104c466e4d72fe724e51cc8a9
JyothsnaSomesh <joesomesh@gmail.com>
2023-12-16 22:31:57 -0600

    new test

--- 07-data-types.sh
+++ 07-data-types.sh
@@ -9,3 +9,9 @@ number10=$10
 sum=$(($number1+$number2+$number3+$number4+$number10))
 
 echo "Total::$sum"
+
+echo "How many args passed:: $#"
+
+echo "All  args passed:: $@"
+
+echo "Script name:: $0"
