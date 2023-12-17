c8f465838913dfa8a31833bba808a8407111818f
JyothsnaSomesh <joesomesh@gmail.com>
2023-12-16 22:46:16 -0600

    new test

---
+++ 12-logs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+
+LOGFILE="/temp/$0-$TIMESTAMP.log"
+
+VALIDATE (){
+if [ $1 -ne 0 ]
+then
+   echo "ERROR: $2....FAILURE"
+   exit 1
+else
+   echo "$2.....SUCCESS"
+ fi
+}
+
+if [ $ID -ne 0 ]
+then
+   echo "ERROR :: Please access with root user"
+   exit 1
+else
+   echo "You are root user"
+fi
+
+yum install mysql -y &>> $LOGFILE
+
+VALIDATE $? "Installing Mysql"
+
+yum install git -y &>> $LOGFILE
+
+VALIDATE $? "Installing GIT"
