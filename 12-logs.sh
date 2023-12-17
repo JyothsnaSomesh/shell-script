7f7e80c36db0b6cfd5b8db3eae96d58c9ee6e905
JyothsnaSomesh <joesomesh@gmail.com>
2023-12-16 23:20:09 -0600

    new test

--- 12-logs.sh
+++ 12-logs.sh
@@ -2,16 +2,21 @@
 
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE (){
 if [ $1 -ne 0 ]
 then
-   echo "ERROR: $2....FAILURE"
+   echo "ERROR: $2....$R FAILed $N"
    exit 1
 else
-   echo "$2.....SUCCESS"
+   echo "$2.....$G SUCCESS $N"
  fi
 }
 
